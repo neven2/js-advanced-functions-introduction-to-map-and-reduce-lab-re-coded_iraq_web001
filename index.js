b2c1cf8131019b191a919c817f2d48a28408895f
Neven Bedr <neven2225@gmail.com>
2020-07-09 17:41:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+const mapToNegativize = (array) => {
+  return array.map(m=> m *-1)
+}
+
+const mapToNoChange = (array) => {
+  return array.map(m=> m)
+}
+
+
+const  mapToDouble =(array) => array.map(m => m * 2)
+const  mapToSquare =(array) => array.map(m => m * m)
+
+function reduceToTotal(array, startingPoint = 0){
+  return array.reduce((a, c)=>{
+    a = a + c
+    return a
+  }, startingPoint)
+}
+
+
+
+const reduceToAllTrue = (array) => array.reduce((accum, curret)=> {
+
+  if(!curret && accum){
+    accum = false
+  }
+
+  return accum
+}, true)
+
+
+const reduceToAnyTrue = (array) => array.reduce((accum, curret)=> {
+
+  if(!!curret){
+    accum = true
+  }
+
+  return accum
+}, false)
